04165956e3e9b6c90f75029aa8fcad2c91e509e6
Onah Leo Chinagorom <leochinagoromonah@gmail.com>
2022-06-02 12:48:22 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ *Description: using main function, this program prints
+ *Programming is like building a multilingual puzzle
+ * Return: 0 Always 0 (Success)
  */
+ 
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
