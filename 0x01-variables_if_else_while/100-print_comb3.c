71627b305fc410a0d40f3da090f4838049f84193
Onah Leo Chinagorom <leochinagoromonah@gmail.com>
2022-06-03 23:58:15 -0700

    combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,12 +19,12 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;	
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
 	}
-
-	putchar('\n');
-
-	return (0);
 }
+
+putchar('\n');
+
+return (0);
+
