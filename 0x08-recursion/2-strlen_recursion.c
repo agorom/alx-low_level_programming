fb2e74c8c6277587d60179fcb68ba2193043ad23
Onah Leo Chinagorom <leochinagoromonah@gmail.com>
2022-06-30 02:54:31 -0700

    legnt of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: argument to the function
+ *
+ * Description: return the required result
+ *
+ * Return: return integer value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	} else
+		return (0);
+}
