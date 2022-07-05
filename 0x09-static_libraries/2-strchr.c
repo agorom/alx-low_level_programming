2a9de196a624d65f786c95d0709e9175185a46c0
Onah Leo Chinagorom <leochinagoromonah@gmail.com>
2022-07-05 00:11:29 -0700

    recovered repo

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,14 +1,6 @@
 #include "main.h"
-#include <stddef.h>
-/**
- * _strchr - return the first occurrence to the string s
- * @s: first argument
- * @c: second argument
- *
- * Description: return the required result
- *
- * Return: return a char pointer
- */
+
 char *_strchr(char *s, char c)
 {
-	return (s);
+	return (0);
+}
