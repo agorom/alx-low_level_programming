977c3175f0771f846cab2297d28b5151dedd5d95
Onah Leo Chinagorom <leochinagoromonah@gmail.com>
2022-07-04 23:34:01 -0700

    dog.h file

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,23 +1,28 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog -Dog data.
- * @name: Name of the dog
- * @age: age of the dog.
- * @owner: owner of the dog.
- * Desctiption: a structure with the name, age and owner of a dog.
+ * struct dog - datatype of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: use to group different datatypes together
  */
-
-typedef struct dog
+struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-} dog_t;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
-char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s);
+
 #endif
