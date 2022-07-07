d3c048bfdf1d714b89827f73ea55b74010bcbc1a
Onah Leo Chinagorom <leochinagoromonah@gmail.com>
2022-07-07 15:32:32 -0700

    calc.h file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,22 +1,30 @@
-#ifndef _CALC_H_
-#define _CALC_H_
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
-
+#ifndef CALC_H
+#define CALC_H
 /**
- * struct op - Struct op
+ * struct op - operation to the performed by the program
+ * @op: string pointer
+ * @f: pointer to functions
  *
- * @op: The operator
- * @f: The function associated
+ * Description: return the required result
+ *
+ * Return: return;
  */
-
 typedef struct op
 {
 	char *op;
-	int (*f)(int a, int b);
+	int (*f)(int, int);
 } op_t;
+
+int (*get_op_func(char *s))(int, int);
+
+int op_add(int, int);
+
+int op_sub(int, int);
+
+int op_div(int, int);
+
+int op_mod(int, int);
+
+int op_mul(int, int);
+
 #endif
